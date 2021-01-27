fd90143c8b121ffff3dbbde3547bddcf4f17d8c8
Raúl Bernalte Sánchez <raulbs7>
2021-01-27 15:03:54 +0100

    Como escribir receta primer boceto

--- gachaneitor/web/antlr/GachaneitorErrorListener.py
+++ gachaneitor/web/antlr/GachaneitorErrorListener.py
@@ -1,8 +1,9 @@
 from antlr4.error.ErrorListener import ErrorListener
 from textblob import TextBlob
 
+
 class GachaneitorErrorListener(ErrorListener):
 
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         mensaje = str(TextBlob(msg).translate(to='es'))
-        raise Exception("Linea: " + str(line) + ", columna: " + str(column) + ". " + mensaje.capitalize() )
+        raise Exception("Linea: " + str(line) + ", columna: " + str(column) + ". " + mensaje.capitalize())
